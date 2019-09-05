f2158bcd37acce72eee21995a96bb382cddb5d19
laixiaoyin <1012551253@qq.com>
2019-09-05 16:13:22 +0800

    web3 project about sign

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,52 @@
-<template>
+<template >
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+      <div id="box">
+        <input type="text" id="information" v-model="msg">
+        <button @click="sign">sign</button>
+        <div id="show"></div>
+      </div>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+#box{
+        width:600px;
+        height:400px;
+        border:1px solid #eee;
+        margin:50px auto;
+        input[type="text"]{
+        width:400px;
+        height:30px;
+        margin:50px;
+        display:block;
+        }
+        button{
+            margin-left: 50px;
+        }
+        #show{
+            width:460px;
+            margin:0 0 50px 50px;
+            height:160px;
+            border:1px solid #999;
+            padding:20px;
+        }
 }
 </style>
+<script>
+module.exports = {
+   name:"app",
+   data(){
+     return {
+       msg:""
+     }
+   },
+   methods:{
+     sign(){
+        var web3 = new Web3(Web3.givenProvider || 'ws://some.local-or-remote.node:8546');
+        var dataToSign = this.msg;
+        web3.eth.sign(web3.fromUtf8(dataToSign), web3.eth.defaultAccount)
+        .then(console.log);
+      }
+   } 
+}
+</script>
